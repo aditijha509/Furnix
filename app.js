9a1eb2c84ed9a1c6b0f1ef4ec97080b418d20831
Rasaghnya Charuhasa <ramyaarji1@gmail.com>
2026-05-18 00:25:35 +0530

    Added feature:fav-icon

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const menu = document.getElementById("menu")
 const list = document.getElementById("list")
 const bars = document.getElementsByClassName("fa-bars")
 
-
 menu.addEventListener('click', () => {
     list.classList.toggle("navList-active");
     bars.classList.toggle("fa-xmark");
 })
+
